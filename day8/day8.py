2fa30edb507af642e67f9ac6b0813119e6e6b15d
Ian Haggerty <ianhaggerty3@gmail.com>
2023-12-29 18:19:24 -0500

    Finally finish day 8

--- day8/day8.py
+++ day8/day8.py
@@ -1,21 +1,6 @@
 import sys
 from itertools import cycle, islice
-from functools import reduce
 from math import gcd
-import math
-from operator import mul
-
-
-with open(sys.argv[1], 'r') as fid:
-    lines = [line.strip() for line in fid.readlines()]
-
-instructions = lines[0]
-
-node_map = {}
-for entry in lines[2::]:
-    exec(f'node_map{entry}')
-
-currents = [node for node in node_map.keys() if node.endswith('A')]
 
 def get_next(node, instruction):
     if instruction == 'L':
@@ -27,18 +12,15 @@ def get_cycle_info(original_node):
     offset_map = {}
     steps = 0
     current = original_node
+    offset_map[(current, 0)] = 0
     for instruction in cycle(instructions):
         current = get_next(current, instruction)
         steps += 1
         key = (current, steps % len(instructions))
         if key in offset_map:
+            print(key[0], offset_map[key], steps - offset_map[key])
             return key[0], offset_map[key], steps - offset_map[key]
         offset_map[key] = steps
-
-cycle_info = [get_cycle_info(node) for node in currents]
-lengths = [item[2] for item in cycle_info]
-print(lengths)
-overall_cycle = reduce(mul, lengths) // reduce(gcd, lengths)
 
 def get_end_states(key, start, length):
     current = key
@@ -53,21 +35,35 @@ def get_end_states(key, start, length):
     
     return ret
 
+def combine_cycles(a, b):
+    c1, l1 = a
+    c2, l2 = b
+    total_cycle = gcd(l1, l2)
+    all_a = {c1 + l1 * i for i in range(l2 // total_cycle)}
+    all_b = {c2 + l2 * i for i in range(l1 // total_cycle)}
+    intersection = all_a.intersection(all_b).pop()
+
+    return (intersection, (l1 * l2) // total_cycle)
+
+with open(sys.argv[1], 'r') as fid:
+    lines = [line.strip() for line in fid.readlines()]
+
+instructions = lines[0]
+
+node_map = {}
+for entry in lines[2::]:
+    exec(f'node_map{entry}')
+
+currents = [node for node in node_map.keys() if node.endswith('A')]
+
+cycle_info = [get_cycle_info(node) for node in currents]
 final_sets = [get_end_states(item[0], item[1], item[2]) for item in cycle_info]
+offset_and_length = [(info[2], info[2]) for info in cycle_info]
 
-def expand_set(final_set, length, count):
-    ret = set()
-    for number in final_set:
-        for i in range(count):
-            ret.add(number + i * length)
-    
-    return ret
+while len(offset_and_length) > 1:
+    a = offset_and_length.pop(0)
+    b = offset_and_length.pop(0)
+    offset_and_length.append(combine_cycles(a, b))
 
-print(overall_cycle)
+print(offset_and_length.pop())
 
-expanded_final_sets = [expand_set(item[0], item[1], overall_cycle // item[1]) for item in zip(final_sets, lengths)]
-
-print(expanded_final_sets)
-
-valid_final_combos = reduce(lambda a, b: a.intersection(b), expanded_final_sets)
-print(min(valid_final_combos))
