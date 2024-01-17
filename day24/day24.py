026c0c3c24edc10a9d3023557ac5d5a75e462d8c
Ian Haggerty <ianhaggerty3@gmail.com>
2024-01-16 22:03:47 -0500

    Wow, that was a convoluted way of accomplishing part 1 I knew what I wanted my algorithm to be, but I had to write way more code than I wanted to actually do it.

--- day24/day24.py
+++ day24/day24.py
@@ -8,42 +8,48 @@ start, stop = list(map(int, lines[0].strip().split(' ')))
 
 def calculate_intersection(p1, p2, v1, v2):
     lhs = p1[1] - p2[1] + (v1[1] * (p2[0] - p1[0])) / v1[0]
-    divisor = (v2[1] - (v1[1] / v1[0]))
+    divisor = (v2[1] - (v1[1] * v2[0] / v1[0]))
 
     if divisor == 0:
         return None, None
 
     t2 = lhs / divisor
 
+    if t2 < 0:
+        return None, None
+    
+    t1 = (p2[0] - p1[0] + v2[0] * t2) / v1[0]
+
+    if t1 < 0:
+        return None, None
+
     x = p2[0] + v2[0] * t2
     y = p2[1] + v2[1] * t2
 
     return x, y
 
+def in_range(x):
+    return x >= start and x <= stop
+
 # todo: fix heuristic to determine if particle collides with test area at all
 def determine_intersects(p, v):
-    x1, y1 = calculate_intersection(p, [start, 0, 0], v, [0, 1, 0])
-    x2, y2 = calculate_intersection(p, [stop, 0, 0], v, [0, 1, 0])
+    t1 = (start - p[0]) / v[0]
+    y1 = p[1] + v[1] * t1
 
-    if x1 is None or x2 is None:
-        print(f'returning False for {p}')
+
+    t2 = (stop - p[0]) / v[0]
+    y2 = p[1] + v[1] * t2
+
+    if t1 < 0 and t2 < 0:
         return False
     
-    print('made it here')
+    if t1 < 0:
+        return not (p[1] < start and y2 < start or p[1] > stop and y2 > stop)
 
-    # if x1 < start and x2 < start:
-    #     return False
+    if t2 < 0:
+        return not (p[1] < start and y1 < start or p[1] > stop and y1 > stop)
     
-    # if x1 > stop and x2 > stop:
-    #     return False
-    
-    if y1 < start and y2 < start:
-        return False
-    
-    if y1 > stop and y2 > stop:
-        return False
-
-    return True
+    return not (y1 < start and y2 < start or y1 > stop and y2 > stop)
 
 positions = []
 velocities = []
@@ -54,7 +60,7 @@ for line in lines[1::]:
     positions.append(position)
     velocities.append(velocity)
 
-valid_indices = filter(lambda i: determine_intersects(positions[i], velocities[i]), range(len(positions)))
+valid_indices = list(filter(lambda i: determine_intersects(positions[i], velocities[i]), range(len(positions))))
 
 total = 0
 for s1, s2 in combinations(valid_indices, 2):
@@ -65,7 +71,7 @@ for s1, s2 in combinations(valid_indices, 2):
 
     x, y = calculate_intersection(p1, p2, v1, v2)
 
-    if x in range(start, stop + 1) and y in range(start, stop + 1):
+    if x is not None and y is not None and in_range(x) and in_range(y):
         total += 1
 
 print(total)
